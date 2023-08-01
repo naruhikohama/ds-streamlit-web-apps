7876eb8784783110351819d95922134b02070a5a
naruhikohama <naruhikohama@gmail.com>
2023-07-31 21:08:18 -0300

    strealit text elements

---
+++ 2_api/text_elements.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+# title
+st.title("Using st.title(), yout can display the text in the title format")
+
+# header
+st.header("The text inside st.header() is in header formatting")
+
+# subheader
+st.subheader()
