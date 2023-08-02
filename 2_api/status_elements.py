0c7f8ef39a3b5a8da624ae06a10b6a50f824451c
naruhikohama <naruhikohama@gmail.com>
2023-08-02 00:19:04 -0300

    streamlit status messages

--- 2_api/status_elements.py
+++ 2_api/status_elements.py
@@ -8,7 +8,7 @@ st.caption('Display a progress bar')
 
 def progress_bar():
     for pct_complete in range(1, 101):
-        time.sleep(0.25)
+        time.sleep(0.1)
         pct_complete = min(pct_complete, 100)
         my_bar.progress(pct_complete)
 
@@ -17,3 +17,20 @@ def progress_bar():
 with st.spinner("Something is processing..."):
     my_bar = st.progress(0)
     progress_bar()
+
+# info
+st.subheader('st.info')
+st.info('This is an information message')
+
+st.subheader('st.success')
+st.success('This is a success message')
+
+st.subheader('st.warning')
+st.warning('This is a warning message')
+
+st.subheader('st.error')
+st.error('This is an error message')
+
+time.sleep(2)
+
+st.balloons()
