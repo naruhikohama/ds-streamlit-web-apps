ba4dfbaed535872a5e53df1357f323f4adc6e7be
naruhikohama <naruhikohama@gmail.com>
2023-08-02 00:12:18 -0300

    streamlit spinner loading

--- 2_api/status_elements.py
+++ 2_api/status_elements.py
@@ -4,8 +4,16 @@ import time
 st.header('st.progress')
 st.caption('Display a progress bar')
 
-my_bar = st.progress(0)
+# my_bar = st.progress(0)
 
-for pct_complete in range(1, 101):
-    time.sleep(0.5)
-    my_bar.progress(pct_complete)
+def progress_bar():
+    for pct_complete in range(1, 101):
+        time.sleep(0.25)
+        pct_complete = min(pct_complete, 100)
+        my_bar.progress(pct_complete)
+
+# spinner
+
+with st.spinner("Something is processing..."):
+    my_bar = st.progress(0)
+    progress_bar()
