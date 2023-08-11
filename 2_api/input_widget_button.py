58d04f294c139f88d55c3146eafe44b3a4307c41
naruhikohama <naruhikohama@gmail.com>
2023-08-11 00:32:02 -0300

    adding checkbox and retrieving its info

--- 2_api/input_widget_button.py
+++ 2_api/input_widget_button.py
@@ -17,3 +17,37 @@ button = st.button('Display random 5 rows')
 if button:
     sample = display_data_random(data)
     st.dataframe(sample)
+
+
+# checkbox
+st.markdown('---')
+st.subheader('Checkbox: st.checkbox')
+
+agree = st.checkbox('I agree to terms and conditions')
+st.write('checkbox status: ', agree)
+
+# mulitple checkboxes
+st.subheader('Multiple checkboxes')
+with st.container():
+    st.info('What technologies you are involved with right now:')
+
+    python = st.checkbox('Python')
+    data_science = st.checkbox('Data Science')
+    ai_ml = st.checkbox('AI - ML')
+    r = st.checkbox('R')
+    julia = st.checkbox('Julia')
+    javascript = st.checkbox('Javascript')
+
+    tech_button = st.button('Submit')
+
+    if tech_button:
+        tech_dict = {
+            'Python': python,
+            'Data Science': data_science,
+            'AI - ML': ai_ml,
+            'R': r,
+            'Julia': julia,
+            'Javascript': javascript
+        }
+
+        st.json(tech_dict)
