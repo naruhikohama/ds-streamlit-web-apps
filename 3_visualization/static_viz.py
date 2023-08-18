1bda9c4b9d4f40a28e02aafa1a31c68035baca03
naruhikohama <naruhikohama@gmail.com>
2023-08-18 08:58:03 -0300

    simple pie chart

--- 3_visualization/static_viz.py
+++ 3_visualization/static_viz.py
@@ -8,3 +8,16 @@ st.title('Static visualization in streamlit with matplotlib and seaborn')
 
 df = pd.read_csv('tips.csv')
 st.dataframe(df)
+
+st.markdown('---')
+with st.container():
+    st.write('Find the number of Male customers (pie and bar chart)')
+    value_counts = df['sex'].value_counts()
+
+    fig, ax =  plt.subplots(figsize=(7, 7))
+    ax.pie(value_counts, 
+           autopct='%0.2f', 
+           labels=value_counts.index)
+    
+    st.pyplot(fig)
+
