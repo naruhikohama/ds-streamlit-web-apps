d0b514db8c85f59d15a45b76a2c6e5fe009d4600
naruhikohama <naruhikohama@gmail.com>
2023-08-19 21:03:48 -0300

    charts side by side

--- 3_visualization/static_viz.py
+++ 3_visualization/static_viz.py
@@ -26,6 +26,30 @@ with st.container():
     ax.bar(value_counts.index, value_counts) 
     st.pyplot(fig) 
 
+st.markdown('---')
+
+with st.container():
+    col1, col2 = st.columns(2) 
     
+    with col1:
+
+        st.subheader('Pie chart')
+        fig, ax =  plt.subplots()
+        ax.pie(value_counts, 
+        autopct='%0.2f', 
+        labels=value_counts.index)
+    
+        st.pyplot(fig)
+
+    with col2:
+        st.subheader('Bar chart')
+        fig, ax = plt.subplots(figsize = (6,6.2)) 
+        ax.bar(value_counts.index, value_counts) 
+        st.pyplot(fig) 
+
+    with st.expander('Click here to display count'):
+        st.dataframe(value_counts)
+
+      
 
 
