c3f3b08ce7d061c4181b824078f0e868ebde6272
naruhikohama <naruhikohama@gmail.com>
2023-08-20 09:17:03 -0300

    Interactive charts with input widgets

--- 3_visualization/static_viz.py
+++ 3_visualization/static_viz.py
@@ -43,7 +43,7 @@ with st.container():
 
     with col2:
         st.subheader('Bar chart')
-        fig, ax = plt.subplots(figsize = (6,6.2)) 
+        fig, ax = plt.subplots(figsize = (6, 6.2)) 
         ax.bar(value_counts.index, value_counts) 
         st.pyplot(fig) 
 
@@ -51,5 +51,28 @@ with st.container():
         st.dataframe(value_counts)
 
       
+st.markdown("---")
+st.subheader('Interactive charts with input widgets')
 
+data_types = df.dtypes 
+cat_cols = tuple(data_types[data_types == 'object'].index)
 
+with st.container():
+    feature = st.selectbox('Select the feature yout want to display in the charts:',
+                           cat_cols)
+    value_counts = df[feature].value_counts()
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader('Pie chart')
+        fig, ax = plt.subplots()
+        ax.pie(value_counts,
+        autopct = '%0.2f',
+        labels = value_counts.index) 
+        st.pyplot(fig)
+    
+    with col2:
+        st.subheader('Bar chart')
+        fig, ax = plt.subplots(figsize = (6, 6.2)) 
+        ax.bar(value_counts.index, value_counts) 
+        st.pyplot(fig) 
