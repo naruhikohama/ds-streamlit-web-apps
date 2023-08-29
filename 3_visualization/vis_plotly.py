12fb9c48b5320e9d6df3ae1d7b72bb38675cea0a
naruhikohama <naruhikohama@gmail.com>
2023-08-28 21:23:23 -0300

    combining plotly with streamlit widgets

--- 3_visualization/vis_plotly.py
+++ 3_visualization/vis_plotly.py
@@ -18,3 +18,12 @@ st.subheader('Draw histogram for total bill by sex')
 fig = px.histogram(data, x = 'total_bill', color = 'sex')
 
 st.plotly_chart(fig)
+
+st.markdown('---')
+st.subheader('Draw a hsitogram for total bill and color by (\'sex, smoker, day, time\')')
+
+select = st.selectbox('Select the category to color', 
+                      ('sex', 'smoker', 'day', 'time'))
+
+fig = px.histogram(data_frame=data, x = 'total_bill', color = select)
+st.plotly_chart(fig)
