c8fe57e9c83a6da5889260a48b36ef07a7cd6a8f
naruhikohama <naruhikohama@gmail.com>
2023-09-02 10:21:15 -0300

    interactive bokeh charts with streamlit widgets

--- 3_visualization/vis_bokeh.py
+++ 3_visualization/vis_bokeh.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 import pandas as pd 
 from bokeh.plotting import figure 
+from bokeh.transform import factor_cmap
 
 st.markdown('# Plots with bokeh')
 
@@ -13,4 +14,28 @@ p = figure(title='Simple line chart',
 
 p.line(x, y, line_width=2)
 p.circle(x, y, size = 8)
+st.bokeh_chart(p)
+
+st.markdown('---')
+st.markdown('## Plotting with dataframes on bokeh')
+
+data = pd.read_csv('tips.csv')
+
+st.subheader('Scatter plot with total bill and tips')
+
+p = figure(title = 'Scatter plot between total bill and tips')
+p.circle(x = 'total_bill', y = 'tip', source = data, size = 5)
+st.bokeh_chart(p)
+
+st.markdown('---')
+st.subheader('Scatter plot with total bill and tip, coloring by options')
+
+p = figure(title = 'Scatter plot coloring by categories')
+select = st.selectbox('Select categories', ('sex', 'smoker', 'day', 'time'))
+color_palette = ['blue', 'red', 'green', '#d35400', 'black']
+
+unique_cats = data[select].unique()
+index_cmap = factor_cmap(select, color_palette[:len(unique_cats)], sorted(unique_cats))
+
+p.circle('total_bill', 'tip', source = data, fill_color=index_cmap, size=5, legend=select, color=index_cmap)
 st.bokeh_chart(p)
