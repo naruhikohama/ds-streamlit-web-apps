66b790f031d23b031065f1a7b98fd35d92776257
naruhikohama <naruhikohama@gmail.com>
2023-09-09 16:51:56 -0300

    using widgets with data profiling app

--- streamlit-project/app.py
+++ streamlit-project/app.py
@@ -11,6 +11,30 @@ st.set_page_config(page_title='Data Profiler', layout='wide')
 # sidebar 
 with st.sidebar:
     uploaded_file = st.file_uploader("Upload data file (csv or xlsx file), not exceeding 10MB") 
+
+    if uploaded_file is not None:
+        st.write('Modes of operation')
+
+        minimal = st.checkbox('Minimal report')
+        dark_mode = False
+        orange_mode = False
+
+        if not minimal:
+
+            display_mode = st.radio('Display mode:', ('Primary', 'Dark', 'Orange'))
+
+            if display_mode == 'Dark':
+                dark_mode = True
+                orange_mode = False
+            
+            elif display_mode == 'Orange':
+                dark_mode = False
+                orange_mode = True
+            
+            else:
+                dark_mode = False
+                orange_mode = False
+    
     
 if uploaded_file is not None:
     # time being, load csv
@@ -19,7 +43,11 @@ if uploaded_file is not None:
     # generate report
 
     with st.spinner('Generating Report'):
-        pr = ProfileReport(df)
+        pr = ProfileReport(df, 
+                           minimal = minimal, 
+                           dark_mode=dark_mode, 
+                           orange_mode=orange_mode
+                           )
 
     st_profile_report(pr)
 
